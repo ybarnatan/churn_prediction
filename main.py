23596cf67ea5fa97d12f731624c4c36aa9b1a5d4
Yair Barnatan <59892775+ybarnatan@users.noreply.github.com>
2025-09-29 11:50:53 -0300

    Printeo df en main

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-def main():
-    print("Hola mundo desde main!")
-
-if __name__ == "__main__": # Asegura que solo se ejecute main() si corremos "python main.py" en terminal, pero no ejecute main() si lo importamos en otro archivo.
-    main()
 import pandas as pd
 import os
 import datetime
@@ -12,7 +7,7 @@ import logging
 #Log para el main
 os.makedirs("logs", exist_ok=True) #Corroboro que exista la carpeta logs
 fecha = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-nombre = f"logs/log_fecha_{fecha}.log"
+nombre = f"log_fecha_{fecha}.log"
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -20,18 +15,17 @@ logging.basicConfig(
     handlers=[
         logging.FileHandler(f"logs/{nombre}", mode="w", encoding="utf-8"),
         logging.StreamHandler()
-    ]
-)
+        ])
 
 logger = logging.getLogger(__name__)
-
 
 
 def main():
     logger.info("Inicio de ejecucion del programa")
     print("Hola mundo desde main!")
-    
-    
+    df = pd.DataFrame({"col1": [1, 2, 3], "col2": [4, 5, 6]}) #DF de ejemplo
+    print(df)
+
     logger.info("Fin de ejecucion del programa")
 
 if __name__ == "__main__": # Asegura que solo se ejecute main() si corremos "python main.py" en terminal, pero no ejecute main() si lo importamos en otro archivo.
