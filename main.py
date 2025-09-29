62ff6ef08b621043526a0281ac4128fe6e9227d2
Yair Barnatan <59892775+ybarnatan@users.noreply.github.com>
2025-09-29 12:23:22 -0300

    Funcion cargar datos fuera del main

--- main.py
+++ main.py
@@ -19,13 +19,26 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
+#Funcion apr cargar  datios
+path = "C:/Users/ybbar/OneDrive/Desktop/DMEyF2025_Competencia01_Proyect_Wedesnday/data/competencia_01_crudo.csv"
+
+def cargar_datos(path: str) -> pd.DataFrame | None:
+    logger.info("Inicio de la funcion cargar_datos")
+    try:
+        df = pd.read_csv(path)
+        logger.info("Datos cargados correctamente con filas: %d y columnas: %d", df.shape[0], df.shape[1])
+        return df
+    except Exception as e:
+        logger.error("Error al cargar los datos: %s", e)
+        raise # Crashear el programa si no cargo el df.
+            
+
 
 def main():
     logger.info("Inicio de ejecucion del programa")
     print("Hola mundo desde main!")
-    df = pd.DataFrame({"col1": [1, 2, 3], "col2": [4, 5, 6]}) #DF de ejemplo
-    print(df)
-
+    df = cargar_datos(path)
+    print(df.head(4))
     logger.info("Fin de ejecucion del programa")
 
 if __name__ == "__main__": # Asegura que solo se ejecute main() si corremos "python main.py" en terminal, pero no ejecute main() si lo importamos en otro archivo.
