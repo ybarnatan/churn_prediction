70ea585abf91e5371344900c57af74ef17e5bd72
Yair Barnatan <59892775+ybarnatan@users.noreply.github.com>
2025-10-03 19:07:49 -0300

    genero conf.yaml

--- src/conf.py
+++ src/conf.py
@@ -1,15 +1,31 @@
-# =====================
-# Configuracion global
-# =====================
-SEMILLA = 811157
-MES_TRAIN = 202102 #Entreno la Bayesiana
-MES_VALIDACION = 202103 # Valido la Bayesiana
-MES_TEST = 202104 #Test final antes de predecir
-GANANCIA_ACIERTO = 780000
-COSTO_ESTIMULO = 20000
+import yaml
+import os
+import logging
 
+# ====================
+# Tomo los datos de conf.yaml
+# ====================
+logger = logging.getLogger(__name__)
 
+#Ruta del archivo de configuracion
+PATH_CONFIG = os.path.join(os.path.dirname(os.path.dirname(__file__)), "conf.yaml")
 
-#Traigo columnas "C" (conteo ), "M" (monto) o "O" (otro)
-import src.identify_traits as Traits
-vars_tipo = Traits.trait_type()
+try: 
+    with open(PATH_CONFIG, "r") as f:
+        _cfgGeneral = yaml.safe_load(f) #Leo el archivo conf.yaml
+        _cfg = _cfgGeneral["competencia01"] #Me quedo con la seccion competencia01
+
+        # Asigno las variables de configuracion:
+        #Los 2do's parametro es el valor por defecto para el yaml.
+        STUDY_NAME = _cfgGeneral.get("STUDY_NAME", "Wendsday")
+        DATA_PATH = _cfg.get("DATA_PATH", "../data/competencia.csv")
+        SEMILLA = _cfg.get("SEMILLA", [42])
+        MES_TRAIN = _cfg.get("MES_TRAIN", "202102")
+        MES_VALIDACION = _cfg.get("MES_VALIDACION", "202103")
+        MES_TEST = _cfg.get("MES_TEST", "202104")
+        GANANCIA_ACIERTO = _cfg.get("GANANCIA_ACIERTO", None)
+        COSTO_ESTIMULO = _cfg.get("COSTO_ESTIMULO", None)
+
+except Exception as e:
+    logger.error(f"Error al cargar el archivo de configuracion: {e}")
+    raise
